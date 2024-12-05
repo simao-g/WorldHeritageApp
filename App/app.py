3341d1fbce3a6369488e15c699039f819c55be56
Simão Gomes <simao2gomes@gmail.com>
2024-12-05 12:25:19 +0000

    V1.0.1

--- App/app.py
+++ App/app.py
@@ -1,4 +1,5 @@
 import warnings
+
 warnings.filterwarnings('ignore', category=FutureWarning)
 from flask import abort, render_template, Flask
 import logging
@@ -6,23 +7,24 @@ import db
 
 APP = Flask(__name__)
 
+
 # Pagina Inicial
 @APP.route('/')
 def index():
     stats = {}
     stats = db.execute('''
         SELECT * FROM
-          (SELECT COUNT(*) id_no FROM World_Heritage_Site) #todos
+          (SELECT COUNT(*) id_no FROM World_Heritage_Site)
         JOIN
-          (SELECT COUNT(*) site_number FROM Location WHERE transboundary=1) #em vários países
+          (SELECT COUNT(*) site_number1 FROM Location WHERE transboundary=1)
         JOIN
-          (SELECT COUNT(*) site_number FROM State_Of_Danger WHERE danger=1) #em perigo atualmente
+          (SELECT COUNT(*) site_number2 FROM State_Of_Danger WHERE danger=1) 
         JOIN 
-          (SELECT COUNT(*) site_number FROM Category WHERE category_short='C') #sites da categoria cultural
+          (SELECT COUNT(*) site_number3 FROM Category WHERE category_short='C')
         JOIN 
-          (SELECT COUNT(*) site_number FROM Category WHERE category_short='C/N') #sites das categorias cultural e natural
+          (SELECT COUNT(*) site_number4 FROM Category WHERE category_short='C/N')
         JOIN 
-          (SELECT COUNT(*) site_number FROM Category WHERE category_short='N') #sites da categoria natural
+          (SELECT COUNT(*) site_number5 FROM Category WHERE category_short='N')
         ''').fetchone()
     logging.info(stats)
     return render_template('index.html', stats=stats)
@@ -32,22 +34,24 @@ def index():
 @APP.route('/sites/')
 def list_sites():
     sites = db.execute(
-      '''
-      SELECT id_no,name_en,short_description_en 
-      FROM World_Heritage_Site
-      ORDER BY id_no
-      ''').fetchall()
+        '''
+        SELECT id_no,name_en,short_description_en
+        FROM World_Heritage_Site
+        ORDER BY id_no
+        ''').fetchall()
     return render_template('sites-list.html', sites=sites)
-
 
 
 @APP.route('/sites/<int:id>/')
 def get_site(id):
     site = db.execute(
         '''
-        SELECT id_no,name_en,short_description_en 
+        SELECT id_no,name_en,short_description_en, GROUP_CONCAT(Place.states_name_en, ', ') as countries
         FROM World_Heritage_Site 
+        JOIN Location ON World_Heritage_Site.id_no = Location.site_number
+        JOIN place ON Location.site_number = place.site_number
         WHERE id_no = ?
+        GROUP BY id_no, name_en, short_description_en
         ''', [id]).fetchone()
 
     if site is None:
@@ -56,7 +60,7 @@ def get_site(id):
     return render_template('site.html', site=site)
 
 
-#pesquisar por pais
+# pesquisar por pais
 @APP.route('/sites/search/<country>')
 def search_sites(country):
     sites = db.execute(
@@ -95,6 +99,7 @@ def transboundary_sites():
     except Exception as e:
         return str(e), 500
 
+
 @APP.route('/transboundary/search/<country>')
 def search_transboundary_sites(country):
     try:
@@ -114,6 +119,7 @@ def search_transboundary_sites(country):
     except Exception as e:
         return str(e), 500
 
+
 @APP.route('/sites/country/<country>')
 def sites_by_country(country):
     sites = db.execute('''
@@ -129,6 +135,7 @@ def sites_by_country(country):
         abort(404, 'No sites found for country: {}'.format(country))
 
     return render_template('sites-by-country.html', country=country, sites=sites)
+
 
 # Run the app
 if __name__ == '__main__':
